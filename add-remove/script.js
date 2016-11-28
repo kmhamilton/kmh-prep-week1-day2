e974a344475f616ac044953c471285d0cc7b4b9d
Kelly Hamilton <hamilton.kellym@gmail.com>
2016-11-27 20:28:04 -0700

    initial commit after file cleanup

---
+++ add-remove/script.js
@@ -0,0 +1,6 @@
+document.getElementById("add-style").addEventListener("click", function() {
+  document.getElementById("message").classList.add("text-style");
+});
+document.getElementById("remove-style").addEventListener("click", function() {
+  document.getElementById("message").classList.remove("text-style");
+});
